646c723076a9ea12adbb004482074aabec5bddca
MatthewMachuca <matthewmachuca457@gmail.com>
2020-12-18 22:57:18 -0800

    DOM exercises

---
+++ domExercises/app.js
@@ -0,0 +1,7 @@
+let ptag = document.createElement('p');
+ptag.textContent = 'Hey i am Red' ;
+ptag.style.color = 'red';
+
+const container = document.querySelector('#container');
+
+container.appendChild(ptag);
